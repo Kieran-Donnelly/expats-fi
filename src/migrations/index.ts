ad66fe2e9f23583156bb854aad4c2af44b1e7a45
Uriah Parish <uriah@podium.dev>
2026-09-25 14:25:40 +0300

    Publish Mik Oy directory listing

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -4,6 +4,7 @@ import * as migration_20260909_070000_publish_pisa_driving_news from './20260909
 import * as migration_20260910_090000_publish_vacek_valmennus from './20260910_090000_publish_vacek_valmennus';
 import * as migration_20260910_220000_publish_vitaldon_therapy from './20260910_220000_publish_vitaldon_therapy';
 import * as migration_20260924_120000_publish_fishing_guide from './20260924_120000_publish_fishing_guide';
+import * as migration_20260925_150000_publish_mik_oy from './20260925_150000_publish_mik_oy';
 import * as migration_20260913_090000_publish_student_kela_news from './20260913_090000_publish_student_kela_news';
 import * as migration_20260907_100000_publish_september_news from './20260907_100000_publish_september_news';
 import * as migration_20260803_075626_initial from './20260803_075626_initial';
@@ -251,5 +252,10 @@ export const migrations = [
     up: migration_20260924_120000_publish_fishing_guide.up,
     down: migration_20260924_120000_publish_fishing_guide.down,
     name: '20260924_120000_publish_fishing_guide',
+  },
+  {
+    up: migration_20260925_150000_publish_mik_oy.up,
+    down: migration_20260925_150000_publish_mik_oy.down,
+    name: '20260925_150000_publish_mik_oy',
   },
 ];
